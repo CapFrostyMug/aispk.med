0e27f59ca05170e7c51c2bc1cbabc96774da0067
CapFrostyMug <nickolas7159@gmail.com>
2023-11-30 12:40:09 +0500

    Development (30.11.23)

--- resources/views/lists/index.blade.php
+++ resources/views/lists/index.blade.php
@@ -11,13 +11,60 @@
 
             @include('session-message')
 
-            <form action="{{ route('students-lists.view-and-print.filtered-list') }}" method="get" class="custom-fn-form">
+            <form action="{{ route('students-lists.view-and-print.filter') }}" method="get" class="custom-fn-form">
                 @csrf
                 @include('lists.search-filter')
             </form>
 
+            <div class="col-12 d-flex justify-content-center my-4">
+                @if(!empty($request))
+                    <span>Найдено записей: {{ $students->count() }}</span>
+                @endif
+            </div>
+
+            {{--@if(isset($students) && $students->isNotEmpty())
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                        <tr class="custom-results-table-bg-color">
+                            <th scope="col" class="text-center">№</th>
+                            <th scope="col" class="text-center">Фамилия</th>
+                            <th scope="col" class="text-center">Имя</th>
+                            <th scope="col" class="text-center">Отчество</th>
+                            <th scope="col" class="text-center">Специальность</th>
+                            <th scope="col" class="text-center">Финансирование</th>
+                            <th scope="col" class="text-center">Зачислен</th>
+                            <th scope="col" class="text-center">Оригиналы</th>
+                            <th scope="col" class="text-center">Ср.&nbsp;балл</th>
+                            <th scope="col" class="text-center">Тест</th>
+                            <th scope="col" class="text-center">Управление</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($students as $student)
+                            <tr>
+                                <th scope="row" class="text-center">{{ $student->id }}</th>
+                                <td class="text-center">{{ $student->surname }}</td>
+                                <td class="text-center">{{ $student->name }}</td>
+                                <td class="text-center">{{ $student->patronymic }}</td>
+                                <td class="text-center text-truncate">{{ $student->faculties()->first()->name }}</td>
+                                <td class="text-center text-truncate">{{ $student->financingTypes()->first()->name }}</td>
+                                <td class="text-center">{{ is_null($student->enrollment->first()->decree_id) ? 'Нет' : 'Да' }}</td>
+                                <td class="text-center">{{ $student->financingTypes()->first()->pivot->is_original_docs ? 'Да' : 'Нет' }}</td>
+                                <td class="text-center">{{ $student->educational()->first()->avg_rating }}</td>
+                                <td class="text-center">{{ $student->educational()->first()->admission_testing ?? '—' }}</td>
+                                <td class="text-center d-flex justify-content-around align-items-center">@include('manage-buttons')</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            @elseif(isset($students) && $students->isEmpty())
+                <p class="fs-5 text-danger mt-5">По выбранным параметрам личные дела не найдены.</p>
+            @endif--}}
+
             @if(isset($students) && $students->isNotEmpty())
-                <table class="table table-bordered table-striped mt-5">
+                <table class="table table-bordered table-striped">
                     <thead>
                     <tr class="custom-results-table-bg-color">
                         <th scope="col" class="col-1 text-center">№</th>
--- resources/views/lists/search-filter.blade.php
+++ resources/views/lists/search-filter.blade.php
@@ -2,13 +2,14 @@
     <div class="col-6">
         <label for="faculty-1" class="form-label fw-bold">Специальность</label>
         <select id="faculty-1" class="form-select" name="faculty">
-            <option value="">Выберите...</option>
+            <option class="custom-fn-reset-filter" value="">Выберите...</option>
             @if(isset($faculties))
                 @foreach($faculties as $item)
                     <option
+                        class="custom-fn-reset-filter"
                         value="{{ $item->id }}"
-                        @if(isset($selectedFaculty) && $selectedFaculty == $item->name)
-                        selected
+                        @if(isset($request['faculty']) && $request['faculty'] == $item->id)
+                            selected
                         @endif>
                         {{ $item->name }}
                     </option>
@@ -20,28 +21,31 @@
         <p class="fw-bold mb-1">Тип финансирования</p>
         <div class="form-check">
             <input id="financing-type-budget-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="financingType[Budget]"
                    value="1"
-                   type="checkbox">
+                   type="checkbox"
+                   @if(isset($request['financingType']['Budget'])) checked @endif>
             <label for="financing-type-budget-1"
                    class="form-check-label">бюджет</label>
         </div>
         <div class="form-check">
             <input id="financing-type-contract-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="financingType[Contract]"
-                   value="1"
-                   type="checkbox">
+                   value="2"
+                   type="checkbox"
+                   @if(isset($request['financingType']['Contract'])) checked @endif>
             <label for="financing-type-contract-1"
                    class="form-check-label">договор</label>
         </div>
         <div class="form-check">
             <input id="financing-type-contract-possible-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="financingType[ContractPossible]"
-                   value="1"
-                   type="checkbox">
+                   value="3"
+                   type="checkbox"
+                   @if(isset($request['financingType']['ContractPossible'])) checked @endif>
             <label for="financing-type-contract-possible-1"
                    class="form-check-label">возможен договор</label>
         </div>
@@ -50,19 +54,21 @@
         <p class="fw-bold mb-1">Статус абитуриента</p>
         <div class="form-check">
             <input id="student-status-enrolled-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="studentStatus[Enrolled]"
                    value="1"
-                   type="checkbox">
+                   type="checkbox"
+                   @if(isset($request['studentStatus']['Enrolled'])) checked @endif>
             <label for="student-status-enrolled-1"
                    class="form-check-label">зачислен</label>
         </div>
         <div class="form-check">
             <input id="student-status-not-enrolled-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="studentStatus[NotEnrolled]"
-                   value="1"
-                   type="checkbox">
+                   value="0"
+                   type="checkbox"
+                   @if(isset($request['studentStatus']['NotEnrolled'])) checked @endif>
             <label for="student-status-not-enrolled-1"
                    class="form-check-label">не зачислен</label>
         </div>
@@ -71,25 +77,30 @@
         <p class="fw-bold mb-1">Тип документов</p>
         <div class="form-check">
             <input id="docs-type-originals-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="docsType[Originals]"
                    value="1"
-                   type="checkbox">
+                   type="checkbox"
+                   @if(isset($request['docsType']['Originals'])) checked @endif>
             <label for="docs-type-originals-1"
                    class="form-check-label">оригиналы</label>
         </div>
         <div class="form-check">
             <input id="docs-type-copies-1"
-                   class="form-check-input"
+                   class="form-check-input custom-fn-reset-filter"
                    name="docsType[Copies]"
-                   value="1"
-                   type="checkbox">
+                   value="0"
+                   type="checkbox"
+                   @if(isset($request['docsType']['Copies'])) checked @endif>
             <label for="docs-type-copies-1"
                    class="form-check-label">копии</label>
         </div>
     </div>
     <div class="col-12 mt-3">
         <button class="col-2 btn btn-success me-3" type="submit">Поиск</button>
-        <button class="col-2 btn btn-secondary custom-fn-reset-form" type="button">Очистить</button>
+        <a class="col-2 btn btn-secondary"
+           href="{{ route('students-lists.view-and-print.filter') }}"
+           role="button">Очистить
+        </a>
     </div>
 </div>
